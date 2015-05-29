2c8ae1541d00877df75eb0f0ec5c3ff60a745cb0
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-05-29 19:31:14 -0400

    make tests recursive

---
+++ test/standards/ngss.science.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('Jeffrey', function() {
+    describe('When coding', function() {
+        it('should write tests', function() {
+            assert.notEqual(null, false);
+        });
+    });
+});
